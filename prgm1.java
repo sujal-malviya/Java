ba9b209fee692083c4548f0857bedd4d8ec62be6
sujal-malviya <sujal.malviya2022@gmail.com>
2025-01-08 12:44:26 +0530

    s

---
+++ prgm1.java
@@ -0,0 +1,11 @@
+public class prgm1 {
+   public static void main(String[] args)
+   {
+   //  System.out.println("*");
+   //  System.out.println("* *");
+   //  System.out.println("* * *");
+   //  System.out.println("* * * *");
+   //  System.out.println("* * * * *");
+   System.out.println("*\n* *\n* * *\n* * * *\n");
+   } 
+}
