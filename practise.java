0463cbae1b6789bd18710ad6b54446ecd0110d7d
sujal-malviya <sujal.malviya2022@gmail.com>
2025-02-24 23:49:07 +0530

    ss

---
+++ practise.java
@@ -0,0 +1,14 @@
+public class practise {
+    public static void main(String[] args) {
+        // int $ = 24;
+        // System.out.println($);
+        byte b = 4;
+        char c = 'a';
+        short s = 512;
+        int i = 1000;
+        float f = 3.14f;
+        double d = 99.9954;
+        System.out.println((f*b)+(i%c)-(d*s));
+        System.out.println("This is a double value");
+    }
+}
