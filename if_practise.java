688f565c55590203b1280684724c0501aa5ac144
sujal-malviya <sujal.malviya2022@gmail.com>
2025-02-25 00:21:38 +0530

    ss

---
+++ if_practise.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class if_practise {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        int n = in.nextInt();
+        if(n>0)
+        {
+            System.out.println(n+" is positive.");
+        }
+        else{
+            System.out.println(n+" is negative.");
+        }
+    }
+}
---
+++ if_practise2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class if_practise2 {
+    public static void main(String[] args) {
+        Scanner in  =  new Scanner(System.in);
+        double temprature =  in.nextDouble();
+        if(temprature >100)
+        {
+            System.out.println(" Fever :- postive");
+        }
+        else
+        {
+            System.out.println("Fever :- negative");
+        }
+    }
+}
