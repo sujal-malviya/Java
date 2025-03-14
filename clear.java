c69f3e240bd53362665482c96d233f7997846ea7
sujal-malviya <sujal.malviya2022@gmail.com>
2025-03-14 21:26:56 +0530

    ss

---
+++ clear.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class clear {
+    public static void main(String []args)
+    {
+        Scanner in = new Scanner(System.in);
+        int X = in.nextInt();
+        int Y = in.nextInt();
+        int days = 7;
+        System.out.println(days-X-Y);
+    }
+}
