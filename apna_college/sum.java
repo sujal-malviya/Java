47c7156bdb04ae690872f0e080f56e62d2286ab8
sujal-malviya <sujal.malviya2022@gmail.com>
2025-03-23 22:23:15 +0530

    ss

---
+++ apna_college/sum.java
@@ -0,0 +1,12 @@
+package apna_college;
+import  java.util.*;
+public class sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("Sum of two numbers is: " + sum);
+    }
+    
+}
