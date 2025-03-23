756171f92adf35f84ad5d0c2cc25203969b24394
sujal-malviya <sujal.malviya2022@gmail.com>
2025-03-23 23:24:30 +0530

    even_num

---
+++ apna_college/prog3.java
@@ -0,0 +1,11 @@
+public class prog3 {
+    public static void main(String[] args) {
+        for(int i = 9;i<=100;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
