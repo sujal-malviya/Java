d2766276bb097bba2f9ca896ea77fc820e37370d
sujal-malviya <sujal.malviya2022@gmail.com>
2025-03-26 22:03:53 +0530

    Arithmatic operator

---
+++ apna_college/Arithmatic.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Arithmatic {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        System.out.println("sum = " +sum);
+    }
+}
