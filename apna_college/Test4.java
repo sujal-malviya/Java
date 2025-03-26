8b391dcfbb22ac7c3ba90798934f4c13a313f96d
sujal-malviya <sujal.malviya2022@gmail.com>
2025-03-26 23:18:08 +0530

    Operator_Question

---
+++ apna_college/Test4.java
@@ -0,0 +1,11 @@
+public class Test4 {
+    public static void main(String[] args)
+    {
+        int x = 9, y = 12;
+        int a = 2, b = 4, c = 6;
+        int exp = 4/3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b*y);
+        System.out.println(exp);
+    }
+}
+// public class Test 
+// {public static void main(String[] args){int x = 10, y = 5;int exp1 = (y * (x / y + x / y));int exp2 = (y * x / y + y * x / y);System.out.println(exp1);System.out.println(exp2);}}kiranbankapur003@gmail.com
