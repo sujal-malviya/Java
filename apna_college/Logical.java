f207e55bf4890e66ed4343b0ce560859bc274f5b
sujal-malviya <sujal.malviya2022@gmail.com>
2025-03-26 22:46:23 +0530

    Logical Operator

---
+++ apna_college/Logical.java
@@ -0,0 +1,13 @@
+
+public class Logical {
+    public static void main(String[] args) {
+        System.out.println( " Logical Operator ");
+        System.out.println(" Logical AND ");
+        System.out.println((2<3)&& (2>3));
+        System.out.println(" Logical OR ");
+        System.out.println((2<3) || (2>3));
+        System.out.println(" Logical NOT ");
+        System.out.println(!(2<3));
+        
+    }    
+}
