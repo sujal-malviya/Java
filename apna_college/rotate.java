a7091c106d2bd882785e18761b31f56369438748
sujal-malviya <sujal.malviya2022@gmail.com>
2025-05-04 23:46:38 +0530

    rotate_array

---
+++ apna_college/rotate.java
@@ -0,0 +1,29 @@
+
+
+class rotate {
+    
+   
+    static void rotateArr(int[] arr, int d) {
+        int n = arr.length;
+  
+        for (int i = 0; i < d; i++) {
+          
+            
+            int first = arr[0];
+            for (int j = 0; j < n - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
+            arr[n - 1] = first;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        int d = 2;
+
+        rotateArr(arr, d);
+
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
